b5c03b50e84b0a008d150b6ee3884313e9c25fcd
quang <thaiquang816@gmail.com>
2019-10-15 17:48:13 +0700

    exercise06

---
+++ bank/UnitTestProject1/unitTestAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bank;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class unitTestAccount
+    {
+        [TestMethod]
+        public void TestMethodAccount()
+        {
+            Account acc = new Account(200000);
+            
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+    }
+}
---
+++ bank/bank/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+   public class Account
+    {
+       private int n;
+       public Account(int amount)
+       {
+            n = amount;
+
+       }
+       public void deposit(int amount)
+       {
+           n = n + amount;
+       }
+       public void Withdraw(int amount)
+       {
+           n = n - amount;
+       }
+       public int Balance()
+       {
+          
+           return n;
+       }
+    }
+}
