428e7e54ead31b03e55481db048fe6508c34a4c4
Quang <thaiquang816@gmail.com>
2019-10-17 14:48:24 +0700

    done test

--- bank/UnitTestProject1/unitTestAccount.cs
+++ bank/UnitTestProject1/unitTestAccount.cs
@@ -15,5 +15,23 @@ namespace UnitTestProject1
             Assert.IsInstanceOfType(acc, typeof(Account));
             Assert.AreEqual(200000, acc.Balance());
         }
+        [TestMethod]
+        public void TestMethodDeposit()
+        {
+            Account acc = new Account(200000);
+            acc.deposit(200000);
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(400000, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodWithDraw()
+        {
+            Account acc = new Account(200000);
+            acc.Withdraw(100000);
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(100000, acc.Balance());
+        }
     }
 }
